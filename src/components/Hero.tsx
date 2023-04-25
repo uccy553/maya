11d2336b8cbd32672e64ae923b5efcba2e677ec6
uche_e <umonday90@proton.me>
2023-04-25 21:44:24 +0100

    hero section added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import styles from '../styles/Hero.module.css';
+import { dog1, dog2, cat1, cat2 } from '@/assets';
+import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className={`${styles.hero}`}>
-       <h1>hero</h1>
-    </div>
+    <>
+        <div className={`${styles.hero}`}>
+            <div className={`${styles.hero_section}`}>
+                <div className={`${styles.hero_text}`}>
+
+                    <h1>Protect Your <span style={{ color: '#5B2585' }}>Furry friend</span> with Pet Insurance Coverage</h1>
+                    <p>Our Insurance is one of the best pet insurance as we offer various policies to help provide financial protection to pet owners by covering the cost of vetinary care for their beloved pets. pet owner can be rest assured as we help assist pet owners choose the policy that best suits their budget and pets health needs. </p>
+                    <div className={`${styles.btn}`}>
+                        <button className={styles.btn1}>Get Started</button>
+                        <button className={styles.btn2}>call</button>
+                    </div>
+                </div>
+
+
+                <div className={`${styles.images}`}>
+                    <div>
+                        <Image src={dog1} alt="dog picture" />
+                        <Image src={cat1} alt="cat image" />
+                    </div>
+
+                    <div>
+                        <Image src={cat2} alt="image of a cat" />
+                        <Image src={dog2} alt="image of a dog" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </>
   )
 }
 
