696618f4aa0d049ecaccfaf16f0cdac39d9a37aa
uche_e <umonday90@proton.me>
2023-04-28 12:28:55 +0100

    refactor

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -22,7 +22,7 @@ const Testimonial = () => {
         dots: true,
         infinite: false,
         speed: 500,
-        slidesToShow: 3.2,
+        slidesToShow: 3,
         slidesToScroll: 3,
         initialSlide: 3,
       
