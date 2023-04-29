66cc5548faf6385990961adf56d612fcd7d9a845
uche_e <umonday90@proton.me>
2023-04-29 10:55:43 +0100

    changes

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,6 +9,20 @@ export { default as More } from "./More";
 export { default as Navbar } from './Navbar';
 export { default as Hero } from './Hero';
 export { default as Testimonial } from './Testimonial';
+export { default as ProductOverview } from './ProductOverview';
+>>>>>>> 1162ea8 (changes)
+<<<<<<< HEAD
+export { default as Navbar } from './Navbar';
+export { default as Hero } from './Hero';
+export { default as Testimonial } from './Testimonial';
 export { default as Services } from './Services';
 export { default as More } from './More';
 export { default as ProductOverview } from './ProductOverview';
+=======
+export { default as Navbar } from "./Navbar";
+export { default as Hero } from "./Hero";
+export { default as Testimonial } from "./Testimonial";
+export { default as ProductOverview } from "./ProductOverview";
+export { default as Services } from "./Services";
+export { default as More } from "./More";
+>>>>>>> 7a6c027 (additions)
