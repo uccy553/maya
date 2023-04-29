6d3f70d1ac9acaf588000be70f8c118b56509e9f
uche_e <umonday90@proton.me>
2023-04-29 09:58:24 +0100

    changes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Navbar } from './Navbar';
 export { default as Hero } from './Hero';
 export { default as Testimonial } from './Testimonial';
+export { default as Services } from './Services';
+export { default as More } from './More';
 export { default as ProductOverview } from './ProductOverview';
